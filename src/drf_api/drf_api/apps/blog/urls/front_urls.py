b41f85a51df1652670b59f879cac4d6709198840
ReZ0iro <rezomoon@gmail.com>
2025-12-28 14:07:29 +0330

    Specified Blog Url

--- src/drf_api/drf_api/apps/blog/urls/front_urls.py
+++ src/drf_api/drf_api/apps/blog/urls/front_urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from drf_api.apps.blog.views import blog_view
+from drf_api.apps.blog.views.front_views import blog_view , blog_detail_view
+
 #
 
 urlpatterns = [
-    path("" , blog_view   )
+    path("" , blog_view   ) , 
+    path("<int:blog_id>/" , blog_detail_view) ,
 ]
